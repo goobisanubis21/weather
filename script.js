8f2544f782586cae1cfb8c81ac3e9cb3ffa66368
Anthony Parrino <anthony21parrino21@gmail.com>
2020-10-16 13:08:54 -0400

    making progress on script file to show city name, temp, humidity and wind. working on uv

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 $(document).ready(function () {
 
-    var searchText = $("#search-sidebar").val();
     var searchButton = $("#search-sidebutton");
-    var APIKey = "e96804d8841b5f95164ad5a431ddd022";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&appid=" + APIKey;
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
 
-        searchButton.on("click", function() {
+    searchButton.on("click", function () {
+
+        var searchText = $("#search-sidebar").val();
+        var temp = $("#temp");
+        var humidity = $("#humidity");
+        var wind = $("#wind");
+        var APIKey = "e96804d8841b5f95164ad5a431ddd022";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&appid=" + APIKey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
             console.log(response);
+            temp.text(Math.round((response.main.temp - 273.15) * 1.80 + 32));
+            humidity.text(response.main.humidity);
+            wind.text(response.wind.speed);
+            // uv.text(response.)
+
         })
-        
+
     });
+
 
 });
