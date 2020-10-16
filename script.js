a1aa84c27bc7c581ce7da6500732d5fb28f77da4
Anthony Parrino <anthony21parrino21@gmail.com>
2020-10-16 11:30:05 -0400

    added contents to index.html file

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
 
+    var searchText = $("#search-sidebar").val();
+    var searchButton = $("#search-sidebutton");
     var APIKey = "e96804d8841b5f95164ad5a431ddd022";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&appid=" + APIKey;
     
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
 
-
+        searchButton.on("click", function() {
+            console.log(response);
+        })
+        
     });
 
 });
