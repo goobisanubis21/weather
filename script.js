7d2271fe54972560175802ac19b7d7f0104cf828
Anthony Parrino <anthony21parrino21@gmail.com>
2020-10-17 23:31:32 -0400

    added 5 day functionality to left bar clicking events and added styling to 5 day forcast divs

--- script.js
+++ script.js
@@ -107,6 +107,40 @@ $(document).ready(function () {
             wind.text(response.wind.speed);
             // uv.text(response.)
 
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+
+            var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon="+ lon + "&exclude={part}&appid=" + APIKey;
+
+            $.ajax({
+                url: queryURL2,
+                method: "GET"
+
+            }).then(function (response) {
+
+                console.log("5-day response: ", response);
+                $("#day1Date").text(moment().add(1, 'days').format("L"));
+                $("#day1Temp").text(Math.round((response.daily[0].temp.day -273.15) * 1.80 + 32) + " F");
+                $("#day1Humidity").text(response.daily[0].humidity + "%");
+
+                $("#day2Date").text(moment().add(2, 'days').format("L"));
+                $("#day2Temp").text(Math.round((response.daily[1].temp.day -273.15) * 1.80 + 32) + " F");
+                $("#day2Humidity").text(response.daily[1].humidity + "%");
+
+                $("#day3Date").text(moment().add(3, 'days').format("L"));
+                $("#day3Temp").text(Math.round((response.daily[2].temp.day -273.15) * 1.80 + 32) + " F");
+                $("#day3Humidity").text(response.daily[2].humidity + "%");
+
+                $("#day4Date").text(moment().add(4, 'days').format("L"));
+                $("#day4Temp").text(Math.round((response.daily[3].temp.day -273.15) * 1.80 + 32) + " F");
+                $("#day4Humidity").text(response.daily[3].humidity + "%");
+
+                $("#day5Date").text(moment().add(5, 'days').format("L"));
+                $("#day5Temp").text(Math.round((response.daily[4].temp.day -273.15) * 1.80 + 32) + " F");
+                $("#day5Humidity").text(response.daily[4].humidity + "%");
+
+            })
+
         })
 
 
